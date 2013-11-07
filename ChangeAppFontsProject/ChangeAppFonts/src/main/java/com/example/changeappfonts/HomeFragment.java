1e17468e33033725963a910b4088287c8d79632f
Raul <raul.gonzalez@ipointsystems.com>
2013-11-07 16:04:10 -0600

    initial commit

---
+++ ChangeAppFontsProject/ChangeAppFonts/src/main/java/com/example/changeappfonts/HomeFragment.java
@@ -0,0 +1,45 @@
+package com.example.changeappfonts;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by raulg on 11/7/13.
+ */
+public class HomeFragment extends Fragment {
+
+    /**
+     * The fragment argument representing the section number for this fragment.
+     */
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    /**
+     * Returns a new instance of this fragment for the given section number.
+     */
+    public static HomeFragment newInstance(int sectionNumber) {
+
+        HomeFragment fragment = new HomeFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public HomeFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.home_fragment, container, false);
+        TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+        textView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
+        return rootView;
+    }
+}
